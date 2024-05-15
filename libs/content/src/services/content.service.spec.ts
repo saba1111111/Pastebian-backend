ec6759f9e78e021235e2bca1ec80437a8defc238
Saba Pachulia <sabapachulia@omegas-MacBook-Pro.local>
2024-05-15 15:51:29 +0400

    add create content method unit tests.

--- libs/content/src/services/content.service.spec.ts
+++ libs/content/src/services/content.service.spec.ts
@@ -1,18 +1,73 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { ContentService } from './content.service';
+import { CONTENT_REPOSITORY_TOKEN } from '../constants';
+import { IContentRepository } from '../interfaces';
+import * as ulidLibrary from 'ulid';
+import { UnexpectedErrorException } from 'libs/common/exceptions';
+
+jest.mock('ulid');
 
 describe('ContentService', () => {
   let service: ContentService;
+  let repository: IContentRepository;
+  let ulid: jest.Mock;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [ContentService],
+      providers: [
+        ContentService,
+        {
+          provide: CONTENT_REPOSITORY_TOKEN,
+          useValue: { create: jest.fn() },
+        },
+      ],
     }).compile();
 
     service = module.get<ContentService>(ContentService);
+    repository = module.get<IContentRepository>(CONTENT_REPOSITORY_TOKEN);
+    ulid = ulidLibrary.ulid as jest.Mock;
   });
 
   it('should be defined', () => {
     expect(service).toBeDefined();
+  });
+
+  describe('create content method tests', () => {
+    it('Should create content', async () => {
+      const expireAt = new Date();
+      const content = 'Mock Content';
+      const id = 'da31';
+
+      ulid.mockReturnValue(id);
+
+      const expireAtInMilliseconds = expireAt.getTime();
+
+      repository.create = jest.fn().mockResolvedValue({
+        content,
+        id,
+        expireAt: expireAtInMilliseconds,
+      });
+
+      const result = await service.create({ expireAt, content });
+      expect(repository.create).toHaveBeenCalledWith({
+        id,
+        content,
+        expireAt: expireAtInMilliseconds,
+      });
+      expect(result).toEqual({
+        id,
+        content,
+        expireAt: expireAtInMilliseconds,
+      });
+    });
+
+    it('Should throw error when providing wrong expireAt argument', async () => {
+      const expireAt = 'wrong Date' as unknown as Date;
+      const content = 'Mock Content';
+
+      expect(service.create({ expireAt, content })).rejects.toThrow(
+        UnexpectedErrorException,
+      );
+    });
   });
 });
--- src/content/content.controller.spec.ts
+++ src/content/content.controller.spec.ts
@@ -1,18 +1,68 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { ContentController } from './content.controller';
+import { ContentService } from 'libs/content';
+import { CreateContentDto } from 'libs/content/dtos';
+import { ContentEntity } from 'libs/content/entities';
 
 describe('ContentController', () => {
   let controller: ContentController;
+  let service: ContentService;
 
   beforeEach(async () => {
+    const serviceMock = {
+      create: jest.fn(),
+    };
+
     const module: TestingModule = await Test.createTestingModule({
       controllers: [ContentController],
+      providers: [
+        {
+          provide: ContentService,
+          useValue: serviceMock,
+        },
+      ],
     }).compile();
 
     controller = module.get<ContentController>(ContentController);
+    service = module.get<ContentService>(ContentService);
   });
 
   it('should be defined', () => {
     expect(controller).toBeDefined();
+  });
+
+  describe('create', () => {
+    it('should call contentService.create with the correct parameters', async () => {
+      const input: CreateContentDto = {
+        expireAt: new Date(),
+        content: 'Mock Content',
+      };
+
+      const result: ContentEntity = {
+        id: 'some-id',
+        content: 'Mock Content',
+        expireAt: input.expireAt.getTime(),
+      };
+
+      jest.spyOn(service, 'create').mockResolvedValue(result);
+
+      const response = await controller.create(input);
+
+      expect(service.create).toHaveBeenCalledWith(input);
+      expect(response).toEqual(result);
+    });
+
+    it('should handle errors correctly', async () => {
+      const input: CreateContentDto = {
+        expireAt: new Date(),
+        content: 'Mock Content',
+      };
+
+      const error = new Error('Some error');
+      jest.spyOn(service, 'create').mockRejectedValue(error);
+
+      await expect(controller.create(input)).rejects.toThrow(error);
+      expect(service.create).toHaveBeenCalledWith(input);
+    });
   });
 });
